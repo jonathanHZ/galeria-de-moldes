5d3c4e25d16a7e5dadc85e36dc086d3c505dc714
Jonathan Hernandez Zapata <jonathan.udem@gmail.com>
2017-03-28 21:47:17 -0500

    restore jenkisfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,24 +7,27 @@
     sh 'npm install'
 }*/
 
-/*pipeline {
+pipeline {
     agent any
     stages {
         stage('Install dependencies') {
             steps {
                 sh 'pwd'
-                sh 'npm install'
-                parallel (
+                sh 'docker build -f /Dockersfiles/node/dockerfile'
+                sh 'docker images'
+                sh 'ls'
+                // sh 'npm install'
+                /*parallel (
                     "Node modules" : { 
                         sh 'npm install'
                     },
                     "Firebase tools" : { 
                         sh "npm install -g firebase-tools" 
                     }
-                )
+                )*/
             }
         }
-        stage('Build') {
+        /*stage('Build') {
             steps {
                 sh "npm run build"
             }
@@ -33,44 +36,6 @@
             steps {
                 sh 'npm run deploy-staging'
             }
-        }
+        }*/
     }
-}*/
-
-node {
-    agent any
-    stages {
-        stage 'build docker image' {
-            try {
-                sh 'docker build -f /Dockersfiles/node/dockerfile'
-                sh 'docker images'
-                sh 'ls'
-                /*workspace = pwd()
-                String script_root = "${workspace}/freebsd-ci"
-
-                if (getBinding().hasVariable("VIEW_SVN")) {
-                    view_svn = VIEW_SVN.toURL()
-                }
-
-                String makeobjdirprefix = "${workspace}/obj"
-
-                if (clean) {
-                    // If the CLEAN build parameter is set in the job,
-                    // then remove all the files in the workspace and exit
-                    sh "sudo chown -R jenkins ."
-                    return deleteDir()
-                }*/
-
-                
-            } finally {
-                    // Send e-mail notifications for failed or unstable builds
-                    /*step([$class: 'Mailer',
-                    notifyEveryUnstableBuild: true,
-                    recipients: "${email_to}",
-                    sendToIndividuals: true])*/
-
-            }
-        }
-    }
-    
 }
