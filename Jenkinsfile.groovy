e4da091a9f06fa75ce3be1d852a64566f8e1d433
Jonathan Hernandez Zapata <jonathan.udem@gmail.com>
2017-03-28 21:35:32 -0500

    Test docker file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,26 +7,21 @@
     sh 'npm install'
 }*/
 
-pipeline {
+/*pipeline {
     agent any
     stages {
-        /*stage('Git checkout') {
-            steps {
-                checkout scm
-            }
-        }*/
         stage('Install dependencies') {
             steps {
                 sh 'pwd'
                 sh 'npm install'
-                /*parallel (
+                parallel (
                     "Node modules" : { 
                         sh 'npm install'
                     },
                     "Firebase tools" : { 
                         sh "npm install -g firebase-tools" 
                     }
-                )*/
+                )
             }
         }
         stage('Build') {
@@ -40,4 +35,41 @@ pipeline {
             }
         }
     }
+}*/
+
+node('galeria-de-moldes') {
+    stages {
+        stage('build docker image') {
+            try {
+                sh 'docker build -f /Dockersfiles/node/dockerfile'
+                sh 'docker images'
+                sh 'ls'
+                /*workspace = pwd()
+                String script_root = "${workspace}/freebsd-ci"
+
+                if (getBinding().hasVariable("VIEW_SVN")) {
+                    view_svn = VIEW_SVN.toURL()
+                }
+
+                String makeobjdirprefix = "${workspace}/obj"
+
+                if (clean) {
+                    // If the CLEAN build parameter is set in the job,
+                    // then remove all the files in the workspace and exit
+                    sh "sudo chown -R jenkins ."
+                    return deleteDir()
+                }*/
+
+                
+            } finally {
+                    // Send e-mail notifications for failed or unstable builds
+                    /*step([$class: 'Mailer',
+                    notifyEveryUnstableBuild: true,
+                    recipients: "${email_to}",
+                    sendToIndividuals: true])*/
+
+            }
+        }
+    }
+    
 }
