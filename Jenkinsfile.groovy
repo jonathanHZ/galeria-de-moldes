91a310a0c89aa49e0876dcd153a83be96e7bb8ae
Jonathan Hernandez Zapata <jonathan.udem@gmail.com>
2017-03-31 23:15:07 -0500

    Add try catch

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,48 +1,58 @@
 #!groovy
-def FIREBASE_TOKEN = "1/isqwPUaI-3A1vyTEcwkHT1ied_mBai_S-E7wz_Tcvck"
-pipeline {
-    agent any
-    stages {
-        stage('Build docker images') {
-            steps {
-                sh 'docker build -t gdm/node ./Dockerfiles/node'
-                sh 'docker build -t gdm/angular-cli ./Dockerfiles/angular-cli'
-                sh 'docker build -t gdm/firebase ./Dockerfiles/firebase'
-            }
+try {
+    currentBuild.result = "SUCCESSFUL"
+    pipeline {
+        agent any
+        stages {
+            stage('Build docker images') {
+                steps {
+                    sh 'docker build -t gdm/node ./Dockerfiles/node'
+                    sh 'docker build -t gdm/angular-cli ./Dockerfiles/angular-cli'
+                    sh 'docker build -t gdm/firebase ./Dockerfiles/firebase'
+                }
 
-        }
-        stage('Install dependencies') {
-            steps {
-                sh 'docker run --user root -dt  --name="gdm_node_${BUILD_ID}" --volume ${WORKSPACE}:/opt/gdm gdm/node bash'
-                sh 'docker exec --user root "gdm_node_${BUILD_ID}" sh -c "cd opt/gdm && npm i"'
-                sh 'docker rm -f "gdm_node_${BUILD_ID}"'
             }
-        }
-        stage('build') {
-            steps {
-                parallel (
-                    "Docker build prod" : { 
-                        sh 'docker run --user root -dt  --name="gdm_ng_build_${BUILD_ID}" --volume ${WORKSPACE}:/opt/gdm gdm/angular-cli bash'
-                        sh 'docker exec --user root "gdm_ng_build_${BUILD_ID}" sh -c "cd opt/gdm && npm run build"'
-                        sh 'docker rm -f "gdm_ng_build_${BUILD_ID}"'
-                    },
-                    "Docker tslint" : { 
-                        sh 'docker run --user root -dt  --name="gdm_ng_lint_${BUILD_ID}" --volume ${WORKSPACE}:/opt/gdm gdm/angular-cli bash'
-                        sh 'docker exec --user root "gdm_ng_lint_${BUILD_ID}" sh -c "cd opt/gdm && npm run lint"'
-                        sh 'docker rm -f "gdm_ng_lint_${BUILD_ID}"'
-                    }
-                )
+            stage('Install dependencies') {
+                steps {
+                    sh 'docker run --user root -dt  --name="gdm_node_${BUILD_ID}" --volume ${WORKSPACE}:/opt/gdm gdm/node bash'
+                    sh 'docker exec --user root "gdm_node_${BUILD_ID}" sh -c "cd opt/gdm && npm i"'
+                    sh 'docker rm -f "gdm_node_${BUILD_ID}"'
+                }
             }
-        }
-        stage('deploy') {
-            steps {
-                sh 'docker run --user root -dt  --name="gdm_firebase_${BUILD_ID}" --volume ${WORKSPACE}:/opt/gdm gdm/firebase bash'
-                sh 'docker exec --user root "gdm_firebase_${BUILD_ID}" sh -c "cd opt/gdm/public && firebase deploy -P staging --token ${FIREBASE_TOKEN}"'
-                sh 'docker rm -f "gdm_firebase_${BUILD_ID}"'
+            stage('build') {
+                steps {
+                    parallel (
+                        "Docker build prod" : { 
+                            sh 'docker run --user root -dt  --name="gdm_ng_build_${BUILD_ID}" --volume ${WORKSPACE}:/opt/gdm gdm/angular-cli bash'
+                            sh 'docker exec --user root "gdm_ng_build_${BUILD_ID}" sh -c "cd opt/gdm && npm run build"'
+                            sh 'docker rm -f "gdm_ng_build_${BUILD_ID}"'
+                        },
+                        "Docker tslint" : { 
+                            sh 'docker run --user root -dt  --name="gdm_ng_lint_${BUILD_ID}" --volume ${WORKSPACE}:/opt/gdm gdm/angular-cli bash'
+                            sh 'docker exec --user root "gdm_ng_lint_${BUILD_ID}" sh -c "cd opt/gdm && npm run lint"'
+                            sh 'docker rm -f "gdm_ng_lint_${BUILD_ID}"'
+                        }
+                    )
+                }
+            }
+            stage('deploy') {
+                steps {
+                    sh 'docker run --user root -dt  --name="gdm_firebase_${BUILD_ID}" --volume ${WORKSPACE}:/opt/gdm gdm/firebase bash'
+                    sh 'docker exec --user root "gdm_firebase_${BUILD_ID}" sh -c "cd opt/gdm/public && firebase deploy -P staging --token \"1/isqwPUaI-3A1vyTEcwkHT1ied_mBai_S-E7wz_Tcvck\"'
+                    sh 'docker rm -f "gdm_firebase_${BUILD_ID}"'
+                }
             }
         }
     }
+} catch (error) { 
+    currentBuild.result = "FAILED"
+    sh 'docker stop $(docker ps -a -q)'
+    sh 'docker rm $(docker ps -a -q)'
+    sh 'exit 1'
+} finally {
+//
 }
+
 /*node {
     stage 'git checkout'
     checkout scm
