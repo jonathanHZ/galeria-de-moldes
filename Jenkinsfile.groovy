a9233ba0d51e2486f243162857737799dc4158bf
Jonathan Hernandez Zapata <jonathan.udem@gmail.com>
2017-04-01 00:04:03 -0500

    Add message

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -49,60 +49,9 @@ try {
                     sh 'docker rm -f "gdm_firebase_${BUILD_ID}"'
                 }
             }
-            stage('Clean environment') {
-                steps {
-                    sh 'docker stop $(docker ps -a -q)'
-                    sh 'docker rm $(docker ps -a -q)'
-                }
-            }
         }
     }
 } catch (error) { 
+    sh 'echo ${error}'
 } finally {
 }
-
-/*node {
-    stage 'git checkout'
-    checkout scm
-
-    stage 'install dependencies'
-    sh 'npm install'
-}*/
-
-/*pipeline {
-    agent any
-    stages {
-        stage('Install dependencies') {
-            steps {
-                docker.image('node:774').inside {
-                    git '…your-sources…'
-                    sh 'mvn -B clean install'
-                }
-                sh 'pwd'
-                sh 'ls'
-                sh 'docker build -t kyani/node ./Dockerfiles/node'
-                sh 'docker images'
-                sh 'ls'
-                sh 'npm install'
-                parallel (
-                    "Node modules" : { 
-                        sh 'npm install'
-                    },
-                    "Firebase tools" : { 
-                        sh "npm install -g firebase-tools" 
-                    }
-                )
-            }
-        }
-        stage('Build') {
-            steps {
-                sh "npm run build"
-            }
-        }
-        stage('Deploy') {
-            steps {
-                sh 'npm run deploy-staging'
-            }
-        }
-    }
-}*/
