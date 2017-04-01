7ba1641aa1f192ea0cc70d4349f9ad8695111fcf
Jonathan Hernandez Zapata <jonathan.udem@gmail.com>
2017-03-31 23:23:27 -0500

    Fix firebase token

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,6 +16,7 @@ try {
                 steps {
                     sh 'docker run --user root -dt  --name="gdm_node_${BUILD_ID}" --volume ${WORKSPACE}:/opt/gdm gdm/node bash'
                     sh 'docker exec --user root "gdm_node_${BUILD_ID}" sh -c "cd opt/gdm && npm i"'
+                    sh 'docker stop "gdm_node_${BUILD_ID}"'
                     sh 'docker rm -f "gdm_node_${BUILD_ID}"'
                 }
             }
@@ -25,11 +26,13 @@ try {
                         "Docker build prod" : { 
                             sh 'docker run --user root -dt  --name="gdm_ng_build_${BUILD_ID}" --volume ${WORKSPACE}:/opt/gdm gdm/angular-cli bash'
                             sh 'docker exec --user root "gdm_ng_build_${BUILD_ID}" sh -c "cd opt/gdm && npm run build"'
+                            sh 'docker stop "gdm_ng_build_${BUILD_ID}"'
                             sh 'docker rm -f "gdm_ng_build_${BUILD_ID}"'
                         },
                         "Docker tslint" : { 
                             sh 'docker run --user root -dt  --name="gdm_ng_lint_${BUILD_ID}" --volume ${WORKSPACE}:/opt/gdm gdm/angular-cli bash'
                             sh 'docker exec --user root "gdm_ng_lint_${BUILD_ID}" sh -c "cd opt/gdm && npm run lint"'
+                            sh 'docker stop "gdm_ng_lint_${BUILD_ID}"'
                             sh 'docker rm -f "gdm_ng_lint_${BUILD_ID}"'
                         }
                     )
@@ -38,7 +41,8 @@ try {
             stage('deploy') {
                 steps {
                     sh 'docker run --user root -dt  --name="gdm_firebase_${BUILD_ID}" --volume ${WORKSPACE}:/opt/gdm gdm/firebase bash'
-                    sh 'docker exec --user root "gdm_firebase_${BUILD_ID}" sh -c "cd opt/gdm/public && firebase deploy -P staging --token \"1/isqwPUaI-3A1vyTEcwkHT1ied_mBai_S-E7wz_Tcvck\"'
+                    sh 'docker exec --user root "gdm_firebase_${BUILD_ID}" sh -c "cd opt/gdm/public && firebase deploy -P staging --token 1/isqwPUaI-3A1vyTEcwkHT1ied_mBai_S-E7wz_Tcvck'
+                    sh 'docker stop "gdm_firebase_${BUILD_ID}"'
                     sh 'docker rm -f "gdm_firebase_${BUILD_ID}"'
                 }
             }
@@ -46,11 +50,11 @@ try {
     }
 } catch (error) { 
     currentBuild.result = "FAILED"
-    sh 'docker stop $(docker ps -a -q)'
-    sh 'docker rm $(docker ps -a -q)'
     sh 'exit 1'
 } finally {
-//
+    sh 'docker stop $(docker ps -a -q)'
+    sh 'docker rm $(docker ps -a -q)'
+    sh 'exit 0'
 }
 
 /*node {
